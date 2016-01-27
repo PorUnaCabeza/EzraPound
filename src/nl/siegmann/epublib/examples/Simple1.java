3dcd6203f5e2b7da5a456d711dce5a44b29acbd8
PorUnaCabeza <hui745158068@gmail.com>
2016-01-28 00:02:10 +0800

    epublib

---
+++ src/nl/siegmann/epublib/examples/Simple1.java
@@ -0,0 +1,49 @@
+package nl.siegmann.epublib.examples;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import nl.siegmann.epublib.domain.Author;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.domain.TOCReference;
+import nl.siegmann.epublib.epub.EpubWriter;
+
+public class Simple1 {
+	public static final String DISK_DIR_ROOT = "C:/book1/";
+	public static void main(String[] args) {
+		try {
+			// Create new Book
+			Book book = new Book();
+	
+			// Set the title
+			book.getMetadata().addTitle("Epublib test book 1");
+			
+			// Add an Author
+			book.getMetadata().addAuthor(new Author("Joe", "Tester"));
+	
+			// Set cover image
+			book.setCoverImage(new Resource(new FileInputStream(new File(DISK_DIR_ROOT + "0.jpg")), "0.jpg"));
+			book.addResource(new Resource(new FileInputStream(new File(DISK_DIR_ROOT + "0.jpg")), "0.jpg"));
+
+			// Add Chapter 1
+			TOCReference t1=book.addSection("introduce", new Resource(new FileInputStream(
+					new File(DISK_DIR_ROOT + "haha.html")), "haha.html"));
+			book.addSection(t1,"11", new Resource(new FileInputStream(
+					new File(DISK_DIR_ROOT + "haha.html")), "haha.html"));
+
+			// Add css file
+			book.getResources().add(new Resource(new FileInputStream(new File(DISK_DIR_ROOT + "haha.html")), "book1.css"));
+
+	
+			// Create EpubWriter
+			EpubWriter epubWriter = new EpubWriter();
+	
+			// Write the Book as Epub
+			epubWriter.write(book, new FileOutputStream(DISK_DIR_ROOT +"test1_book1.epub"));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
